7424000c2e1d7b118a4f688255e5c22502b4332b
makstt <maksattologonovn@gmail.com>
2021-11-22 11:00:01 +0600

    comments put away

--- run.py
+++ run.py
@@ -55,18 +55,18 @@ def remove_dir():
 
 def run():
     make_dir()
-    # print("Парсинг коммерческих машин")
-    # run_commercial()
-    # time.sleep(1)
-    # print("Парсинг House.kg")
-    # run_house_kg()
-    # time.sleep(1)
-    # print("Парсинг легковых машин")
-    # run_mashina_kg()
-    # time.sleep(1)
-    # print("Парсинг запчастей на mashina.kg")
-    # run_mashina_spare()
-    # time.sleep(1)
+    print("Парсинг коммерческих машин")
+    run_commercial()
+    time.sleep(1)
+    print("Парсинг House.kg")
+    run_house_kg()
+    time.sleep(1)
+    print("Парсинг легковых машин")
+    run_mashina_kg()
+    time.sleep(1)
+    print("Парсинг запчастей на mashina.kg")
+    run_mashina_spare()
+    time.sleep(1)
     print("Парсинг машин на lalafo.kg")
     run_lalafo_auto()
     time.sleep(1)
@@ -78,32 +78,32 @@ def run():
     time.sleep(1)
     print("Парсинг смартфонов на lalafo.kg")
     run_lalafo_tel()
-    # time.sleep(1)
-    # pars_url = []
-    # count = int(conf('PAGES_COUNT'))
-    # path, dirs, files = next(os.walk("accounts"))
-    # for file in files:
-    #     with open(f'accounts/{file}', 'r') as f:  # читаем файл с аккаунтами
-    #         for acc in f.readlines():
-    #             pars_url.append(acc.strip('\n'))  # заполняем список
-    #     file_name = file.replace('.txt', '')
-    #     url = config.URL
-    #     login = config.LOGIN
-    #     password = config.PASSWORD
-    #     check_config(url, login, password)
-    #     current_date = datetime.datetime.today()
-    #     parser = Inst(url, login, password)  # Инициализируем класс авторизации
-    #     error = parser.auth_inst()  # Авторизация в инстаграм
-    #     if error != 'error':
-    #         for user_url in pars_url:
-    #             err = parser.scrap_post(user_url, count, current_date, file_name)  # Парсим страницу
-    #             if err == 'error':
-    #                 continue
-    #     else:
-    #         parser.close_browser()
-    #         break
-    #     parser.close_browser()  # закрываем браузер
-    # remove_dir()
+    time.sleep(1)
+    pars_url = []
+    count = int(conf('PAGES_COUNT'))
+    path, dirs, files = next(os.walk("accounts"))
+    for file in files:
+        with open(f'accounts/{file}', 'r') as f:  # читаем файл с аккаунтами
+            for acc in f.readlines():
+                pars_url.append(acc.strip('\n'))  # заполняем список
+        file_name = file.replace('.txt', '')
+        url = config.URL
+        login = config.LOGIN
+        password = config.PASSWORD
+        check_config(url, login, password)
+        current_date = datetime.datetime.today()
+        parser = Inst(url, login, password)  # Инициализируем класс авторизации
+        error = parser.auth_inst()  # Авторизация в инстаграм
+        if error != 'error':
+            for user_url in pars_url:
+                err = parser.scrap_post(user_url, count, current_date, file_name)  # Парсим страницу
+                if err == 'error':
+                    continue
+        else:
+            parser.close_browser()
+            break
+        parser.close_browser()  # закрываем браузер
+    remove_dir()
 
 
 if __name__ == '__main__':
