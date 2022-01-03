bc50abb91a855b588056082ffaec13408ca083ae
sultanfranco98 <s.syidaliev@gmail.com>
2022-01-03 17:11:56 +0600

    change path directory image lalafo service

--- service/lalafo.py
+++ service/lalafo.py
@@ -9,14 +9,14 @@ def save_images_for_lalafo(images: List, key: str, name: str):
     images_list = []
     for i in range(len(images)):
         try:
-            mkdir(f'images/{name}/{key}')
+            mkdir(f'images/{name}/{key[:7]}-{name}')
         except FileExistsError:
             pass
         time.sleep(0.1)
         p = requests.get(images[i])
-        out = open(f"images/{name}/{key}/{i}.jpg", "wb")
+        out = open(f"images/{name}/{key[:7]}-{name}/{i}.jpg", "wb")
         out.write(p.content)
-        with open(f"images/{name}/{key}/{i}.jpg", 'rb') as image:
+        with open(f"images/{name}/{key[:7]}-{name}/{i}.jpg", 'rb') as image:
             images_list.append(image.read())
         out.close()
     return images_list
