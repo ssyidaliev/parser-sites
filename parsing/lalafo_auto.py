a21f5b981b385541b9cf8f4cac35395c38835706
sultanfranco98 <s.syidaliev@gmail.com>
2022-01-03 18:15:22 +0600

    added print for test

--- parsing/lalafo_auto.py
+++ parsing/lalafo_auto.py
@@ -35,10 +35,13 @@ def lalafo_cars(url: str, pages: int):
             options.headless = True
             driver = webdriver.Chrome(executable_path=PATH,
                                       options=options)
+            print("good1")
             try:
                 time.sleep(10)
+                print("good2")
                 driver.get(url=(url + '?page=' + str(page)))
                 time.sleep(10)
+                print("good3")
                 items = driver.find_elements(By.CLASS_NAME, "AdTileHorizontalTitle")
                 try:
                     region = driver.find_element(By.CLASS_NAME, "meta-info__city ").text
@@ -47,6 +50,7 @@ def lalafo_cars(url: str, pages: int):
                 created_at = driver.find_element(By.CLASS_NAME, 'AdTileHorizontalDate').text
                 driver.find_element(By.CLASS_NAME, 'AdTileHorizontalCallBtnTitle').click()
                 time.sleep(10)
+                print("good4")
                 phone_number = driver.find_element(By.CLASS_NAME, 'AdTileHorizontalCallBtnTitle').text
                 for item in items:
                     if 'error' not in item.text and 'after' not in item.text:
